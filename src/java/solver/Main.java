5922c17c4232766ce16e98f7f8e9de7d1c981cba
Zoey Chen <zixiu.zoey.chen@gmail.com>
2026-01-21 20:09:12 -0500

    submission final

--- src/java/solver/Main.java
+++ src/java/solver/Main.java
@@ -5,14 +5,28 @@ public class Main{
     static final String IMAGE_FILE_PATH = "";
 
     public static void main(String[] args) {
-        
-        String[][] up = {{"W", "W", "W", "R"}, {"W", "W", "W", "R"}, {"W", "W", "W", "R"}, {"W", "W", "W", "R"}};
+        /* 
+        String[][] up = {{"W", "O", "R", "R"}, {"W", "O", "R", "R"}, {"W", "O", "R", "R"}, {"W", "O", "R", "R"}};
 
-        String[][] down = {{"Y", "Y", "Y", "O"}, {"Y", "Y", "Y", "O"}, {"Y", "Y", "Y", "O"}, {"Y", "Y", "Y", "O"}};
+        String[][] down = {{"Y", "R", "O", "O"}, {"Y", "R", "O", "O"}, {"Y", "R", "O", "O"}, {"Y", "R", "O", "O"}};
 
-        String[][] front = {{"R", "R", "R", "Y"}, {"R", "R", "R", "Y"}, {"R", "R", "R", "Y"}, {"R", "R", "R", "Y"}};
+        String[][] front = {{"R", "W", "Y", "Y"}, {"B", "B", "B", "B"}, {"R", "W", "Y", "Y"}, {"R", "W", "Y", "Y"}};
 
-        String[][] back = {{"W", "O", "O", "O"}, {"W", "O", "O", "O"}, {"W", "O", "O", "O"}, {"W", "O", "O", "O"}};
+        String[][] back = {{"W", "W", "Y", "O"}, {"G", "G", "G", "G"}, {"W", "W", "Y", "O"}, {"W", "W", "Y", "O"}};
+
+        String[][] left = {{"G", "G", "G", "G"}, {"R", "W", "Y", "Y"}, {"G", "G", "G", "G"}, {"G", "G", "G", "G"}};
+
+        String[][] right = {{"B", "B", "B", "B"}, {"W", "W", "Y", "O"}, {"B", "B", "B", "B"}, {"B", "B", "B", "B"}};
+
+        */
+
+        String[][] up = {{"W", "W", "W", "W"}, {"W", "W", "W", "W"}, {"W", "W", "W", "W"}, {"W", "W", "W", "W"}};
+
+        String[][] down = {{"Y", "Y", "Y", "Y"}, {"Y", "Y", "Y", "Y"}, {"Y", "Y", "Y", "Y"}, {"Y", "Y", "Y", "Y"}};
+
+        String[][] front = {{"R", "R", "R", "R"}, {"R", "R", "R", "R"}, {"R", "R", "R", "R"}, {"R", "R", "R", "R"}};
+
+        String[][] back = {{"O", "O", "O", "O"}, {"O", "O", "O", "O"}, {"O", "O", "O", "O"}, {"O", "O", "O", "O"}};
 
         String[][] left = {{"G", "G", "G", "G"}, {"G", "G", "G", "G"}, {"G", "G", "G", "G"}, {"G", "G", "G", "G"}};
 
@@ -23,9 +37,57 @@ public class Main{
             up, down, front, back, left, right
         };
 
-        Cube testCube  = new Cube(test);
+        Cube testCube = new Cube(test);
 
-        System.out.println(testCube.toStringCubits());
+        //testCube.move_uPrime();
+        //System.out.println(testCube.toString());
+
+         /* 
+        testCube.move_R();
+        testCube.move_r();
+        testCube.move_l();
+        testCube.move_u();
+        testCube.move_f();
+        testCube.move_r();
+        testCube.move_dPrime();
+        testCube.move_X();
+        testCube.move_B();
+        testCube.move_u();
+        */
+
+        testCube.applyMoves("U', L");
+        
+        System.out.println(testCube.toString());
+        
+        testCube.solve();
+
+        
+        //captureFace();
+
+        ArduinoUploader auTurnFront = new ArduinoUploader("C:\\Users\\zixiu\\Projects\\TEJ4M-Rubiks-Cube-Solver\\src\\arduino\\frontFullTurn");
+
+        ArduinoUploader auBlank = new ArduinoUploader("C:\\Users\\zixiu\\Projects\\TEJ4M-Rubiks-Cube-Solver\\src\\arduino\\blank");
+
+        
+
+        try {
+            //captureFace();
+            //Thread.sleep(3000);
+            auTurnFront.runIno();
+            Thread.sleep(3000);
+            auBlank.runIno();
+            //Thread.sleep(3000);
+            //captureFace();
+        } catch (InterruptedException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+
+        
+        
+
+        //GUI gui = new GUI();
+        //gui.launch();
     }
 
     public static void captureFace(){
@@ -36,10 +98,13 @@ public class Main{
             //take photo
             bashCommand("adb shell input keyevent 27");
 
+            //wait a moment
+            Thread.sleep(500);
+
             //identify the photo
             String imgName = bashCommandString("adb shell 'ls -t /sdcard/DCIM/Camera/ | head -n 1'");
 
-            bashCommand("adb pull //sdcard/DCIM/Camera/" + imgName + " \"/C:/Users/zoeyz/Documents/projects/TEJ4M-Rubiks-Cube-Solver/src/images/\"");
+            bashCommand("adb pull //sdcard/DCIM/Camera/" + imgName + " \"/C:/Users/zixiu/projects/TEJ4M-Rubiks-Cube-Solver/src/images/\"");
 
             ColourProcessor cp = new ColourProcessor(imgName);
             cp.test();
